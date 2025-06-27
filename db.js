7a02a99b58447cc6a9665da565ff90cecb8d2a32
anujda09 <anujdamani9@gmail.com>
2025-06-27 11:27:14 +0530

    signup complete

---
+++ db.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.ObjectId;
+
+const User = new Schema({
+    email: {
+        type: String,
+        unique: true
+    },
+    name: String,
+    password: String
+
+});
+const Expense = new Schema({
+    expense: String,
+    expenseType: Boolean,
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+    },
+    amount: Number,
+    reason: String
+
+});
+const UserModel = mongoose.model("users", User);
+const ExpenseModel = mongoose.model("expenses", Expense);
+module.exports = {
+    UserModel,
+    ExpenseModel
+};
--- index.js
+++ index.js
@@ -1,28 +1,84 @@
 const express = require('express');
 const app = express();
 const port = 3012;
-const todoRoutes = require('./todo');
-
+const expenseRoutes = require('./expense');
+const z = require('zod')
+const jwt = require('jsonwebtoken')
+const JWT_SECRET = "ShreeJagnnath"
+const bcrypt = require('bcrypt')
+const {
+    UserModel,
+    ExpenseModel
+} = require('./db');
+const mongoose = require("mongoose");
 
 app.use(express.json());
 
 
-app.use('/todo', todoRoutes);
+app.use('/expense', expenseRoutes);
+
+app.post('/signup', async function(req, res) {
+
+    const requireBody = z.object({
+        email: z.string().min(6).max(50).email(),
+        name: z.string().min(2).max(45),
+        password: z.string().min(3).max(20)
+    });
+    const parsedData = requireBody.safeParse(req.body);
+    if (!parsedData.success) {
+        res.status(400).json({
+            message: "Incorrect format"
+        })
+        return
+    }
+
+    const { email, name, password } = parsedData.data;
+
+    const existinguser = await UserModel.findOne({ email });
+    if (existinguser) {
+        return res.status(409).json({ Message: 'email already exit use signin instead of signup' })
+    }
+    const hashedPassword = await bcrypt.hash(password, 3);
+    try {
+        await UserModel.create({
+            name,
+            password: hashedPassword,
+            email
+        })
+        res.status(200).json({ message: "data added sucessfuly" })
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "cant add data" })
+
+    }
+
+
+});
+
 
 
 app.post('/signin', function(req, res) {
     res.json("signin route working");
 });
 
-app.post('/signup', function(req, res) {
-    res.json("signup route working");
-});
 
 
 function auth(req, res, next) {
 
     console.log("Auth middleware triggered");
     next();
+}
+
+function Incomingrequest(req, res, next) {
+    const log = `[${new Date().toISOString()}] ${req.method} ${req.url}`;
+
+    console.log(log);
+    next();
+}
+
+function RateLimmiter(req, res, next) {
+
 }
 
 
